5c1552d9c0d8d4a44537e5f5e44c76e85075ab8e
camilledelbos <camille.delbos@gmail.com>
2014-04-16 10:52:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capdevant::Application.config.secret_key_base = '62b633a98c5c2db11932d77fc910b4b5d3abf10842d3adf49b4148072bc9b7b0a0b8232df42f084fd8d6c440984c345573c22dfeef7aa9a6828a4ab000cf9a10'
